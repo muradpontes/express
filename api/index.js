a12365f77ec2b7b16a882f13ffa5ea70ca83e220
muradpontes <muradsp20@gmail.com>
2024-03-18 15:48:13 -0300

    fix

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const PDFDocument = require('pdfkit');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3030;
 
 app.use(bodyParser.json());
 
@@ -12,31 +12,38 @@ app.get('/', (req, res) => res.send('home'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const laboratorios = [
-    { id: 1, nome: 'lab I', capacidade: 20, descricao: 'laboratorio 1' },
-    { id: 2, nome: 'lab II', capacidade: 15, descricao: 'laboratorio 2' },
-    { id: 3, nome: 'lab III', capacidade: 30, descricao: 'laboratorio 3' },
-    { id: 4, nome: 'lab IV', capacidade: 10, descricao: 'laboratorio 4' },
-    { id: 5, nome: 'lab V', capacidade: 15, descricao: 'laboratorio 5' },
-    { id: 6, nome: 'lab VI', capacidade: 20, descricao: 'laboratorio 6' },
+    {nome: 'lab I', capacidade: 20, descricao: 'laboratorio 1' },
+    {nome: 'lab II', capacidade: 15, descricao: 'laboratorio 2' },
+    {nome: 'lab III', capacidade: 30, descricao: 'laboratorio 3' },
+    {nome: 'lab IV', capacidade: 10, descricao: 'laboratorio 4' },
+    {nome: 'lab V', capacidade: 15, descricao: 'laboratorio 5' },
+    {nome: 'lab VI', capacidade: 20, descricao: 'laboratorio 6' },
 ];
 
 app.get('/laboratorio/todos', (req, res) => {
     res.json(laboratorios);
 });
 
-app.get('/laboratorio/novo', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'novo.html'));
-});
+app.post("/laboratorio/novo", (req, res) => {
+    const novoLab = req.body;
+  
+    try {
+      if (novoLab) {
+        laboratorios.push(novoLab);
+        res.json({
+        message: "laboratorio adicionado",
+        });
+      } else {
+        throw new Error("indefinido");
+      }
+    } catch (e) {
+      res.json({
+        message: "erro ao adicionar laboratorio",
+        error: e.message,
+      });
+    }
+  });
 
-app.post('/laboratorio/novo', (req, res) => {
-    const resposta = req.body;
-    const id = laboratorios.length > 0 ? laboratorios[laboratorios.length - 1].id + 1 : 1;
- 
-    const novoLab = { id, ...resposta };
-    laboratorios.push(novoLab);
-
-    res.send('laboratorio adicionado com sucesso');
-});
 
 app.get('/laboratorio/relatorio', (req, res) => {
     const doc = new PDFDocument();
@@ -45,7 +52,6 @@ app.get('/laboratorio/relatorio', (req, res) => {
     doc.fontSize(16).text('relatorio de laboratorios\n\n');
 
     laboratorios.forEach(laboratorio => {
-        doc.text(`id: ${laboratorio.id}`);
         doc.text(`nome: ${laboratorio.nome}`);
         doc.text(`capacidade: ${laboratorio.capacidade}`);
         doc.text(`descrição: ${laboratorio.descricao}`);
@@ -58,7 +64,7 @@ app.get('/laboratorio/relatorio', (req, res) => {
 const middleware = (req, res, next) => {
     const horaAtual = new Date().getHours();
     console.log('hora atual:', horaAtual);
-    if (horaAtual >= 8 && horaAtual < 17) {
+    if (horaAtual >= 8 && horaAtual < 24) {
         next();
     } else {
         res.status(403).send('acesso permitido somente entre 08h e 17h');
@@ -69,3 +75,4 @@ app.use('/laboratorio/novo', middleware);
 app.use('/laboratorio/relatorio', middleware);
 
 app.listen(port, () => console.log(`rodando na porta ${port}! http://localhost:${port}/`));
+module.exports = app;   
